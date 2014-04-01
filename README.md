759da3c97cc5294d860597fa016e8adec008a156
Rémi Andrieux <pimolo10300@gmail.com>
2014-04-01 09:15:22 +0200

    readme, class bug fixed

---
+++ README.md
@@ -0,0 +1,19 @@
+ScrollSpy.js by pimolo
+======================
+
+What's that shit ?
+-------------------
+ScrollSpy.js is a plugin for jQuery that spy your scroll to dynamise your menu.
+
+How does it works ?
+-------------------
+It adds a class 'active' on the right link of your menu when you scroll on the target with the id related to.
+
+Requirements
+------------
+Needs jQuery
+
+Releases :
+----------
+
+v1.0 : Works fine, but needs a specific markup
--- scrollspy.js
+++ scrollspy.js
@@ -1,6 +1,12 @@
 $(function() {
     $(window).bind('scroll resize', function() {
         var currentSection = null;
-        if ($(window).) {};
+        $('.section').each(function() {
+          var element = $(this).attr('id');
+          if ($(window).scrollTop() >= $('#' + element).offset().top) {
+            currentSection = element;
+          };
+          $('nav ul li a').removeClass('active').parent().siblings().find('a[href=#' + currentSection + ']').addClass('active');
+        });
     });
 });

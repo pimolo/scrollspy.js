54589fa9c830e7ea972b1ea6bdae94d70cbd365e
Rémi Andrieux <pimolo10300@gmail.com>
2014-06-13 14:40:42 +0200

    params & smooth scrolling

--- scrollspy.js
+++ scrollspy.js
@@ -1,15 +1,38 @@
 (function() {
-  $.fn.spy = function() {
+  $.fn.spy = function(args) {
+    args = $.extend(
+      {
+        class: 'active',
+        scroll: 'smooth'
+      }, args
+    );
     var menu = $(this);
     $(window).bind('scroll resize', function() {
-      var currentSection = null;
+      var currentSection;
       $('.section').each(function() {
         var element = $(this).attr('id');
-        if ($(window).scrollTop() >= $('#' + element).offset().top - $('nav').height()) {
+        if ($(window).scrollTop() >= $('#' + element).offset().top - menu.height()) {
           currentSection = element;
         }
-        menu.removeClass('active').parent().siblings().find('a[href=#' + currentSection + ']').addClass('active');
+        menu.find('a').removeClass(args.class);
+        menu.find('a[href=#' + currentSection + ']').addClass(args.class);
       });
     });
+
+    if (args.scroll == 'smooth') {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top - menu.height()
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    } else { return false; }
+    return this;
   };
 })($);
